0acea68f313e9477c5b208178b107072c912ab22
Aisha Mohamed <ahmohamed2@wisc.edu>
2024-03-16 03:03:57 -0500

    Added documentation for the database client

--- backend/db_client.py
+++ backend/db_client.py
@@ -16,6 +16,31 @@ DB_PASSWORD = os.environ['MYSQL_PASSWORD']
 
 
 class Client:
+    """A class representing the database client.
+
+    Attributes
+    ----------
+    db_host : str, optional
+        the hostname of the MySQL database (default is value of DB_HOST)
+    db_port : str, optional
+        the port of the MySQL database (default is value of DB_PORT)
+    db_name : str, optional
+        the name of the MySQL database (default is value of DB_NAME)
+    user : str, optional
+        the username of the MySQL client (default is value of DB_USER)
+    password : str, optional
+        the password of the MySQL client (default is value of DB_PASSWORD)
+
+    Methods
+    -------
+    create_user(email, username)
+        Returns an SQL query for creating a new user username whose email is email.
+        
+    create_folder(folder_name, username)
+        Returns an SQL query for creating a new folder with name folder_name for user username.
+
+    """
+
     def __init__(self, user=DB_USER, password=DB_PASSWORD, host=DB_HOST, port=DB_PORT, db_name=DB_NAME):
         self.db_host = host
         self.db_port = port
@@ -24,13 +49,33 @@ class Client:
         self.password = password
 
 
+    def create_user(self, email, username):
+        """Returns an SQL query for creating a new user username whose email is email.
 
-    "returns an SQL query for creating a new user"
-    def create_user(self, email, display_name):
-        return f"INSERT INTO User (email, display_name) VALUES ('{email}', '{display_name}')"
+        Parameters
+        ----------
+        email : str
+        username : str
+        
+        Returns
+        -------
+        str
+        """
+        return f"INSERT INTO User (email, display_name) VALUES ('{email}', '{username}')"
 
 
-    def create_folder(self, display_name, owner):
-        return f"INSERT INTO Folder (display_name, owner) VALUES ('{display_name}', '{owner}')"
+    def create_folder(self, folder_name, username):
+        """Returns an SQL query for creating a new folder with name folder_name for user username.
+
+        Parameters
+        ----------
+        folder_name : str
+        username : str
+        
+        Returns
+        -------
+        str
+        """
+        return f"INSERT INTO Folder (display_name, owner) VALUES ('{folder_name}', '{username}')"
 
 
