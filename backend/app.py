e677fd03b2387ea55ec1f87358ee0f4c5d14c855
David Viggiano <dviggiano@wisc.edu>
2024-04-08 19:00:04 -0500

    Add route updates

--- backend/app.py
+++ backend/app.py
@@ -20,12 +20,14 @@ functions:
     * main - executes when the script is run directly; launches the API
 """
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 from flask_mysqldb import MySQL
 from flask_cors import CORS
 
 from db_client import Client
-# TODO create unit tests for routes
+
+NOTE_DATA_PATH = './note-data'
+AUDIO_DATA_PATH = './audio-data'
 
 app = Flask(__name__)
 db = MySQL()
@@ -36,24 +38,24 @@ app.config['MYSQL_HOST'] = client.db_host
 app.config['MYSQL_DB'] = client.db_name
 app.config['MYSQL_PORT'] = client.db_port
 app.config['MYSQL_USER'] = client.user  # TODO create new user
-app.config['MYSQL_PASSWORD'] = client.password
+app.config['MYSQL_ROOT_PASSWORD'] = client.password
 
 db.init_app(app)
 CORS(app)
 
-def execute_query(query):
-    try:
-        cursor = db.connection.cursor()
-        cursor.execute(query)
-        rows = cursor.fetchall()
-        db.connection.commit() # save changes to the database
-        return rows
-    except Exception as e:
-        return 'Failure'
-    finally:
-        cursor.close()
 
-# TODO look into if each route can go into other files/modules
+# def execute_query(query): TODO update routes to leverage DB client
+#     try:
+#         cursor = db.connection.cursor()
+#         query = client.create_user(email, username)
+#         cursor.execute(query)
+#         rows = cursor.fetchall()
+#         db.connection.commit() # save changes to the database
+#     except Exception as e:
+#         return 'Failure'
+#     finally:
+#         cursor.close()
+
 
 @app.route('/user/<email>', methods=['GET'])
 def get_user_data(email):
@@ -63,19 +65,118 @@ def get_user_data(email):
     ----------
     email : str
         The email address of the user to fetch data for.
-    TODO only allow this route to provide data with verification from Duo authentication, rather than taking param
 
     Returns
     -------
     JSON response
         A JSON response containing the user's data, including display name, sequences, and folders.
     """
-    query = client.get_user_data(email)
-    raw_data = execute_query(query)
-    print(raw_data)
-    user_data = {}  # TODO populate from database; data includes display name, folders, and sequences
+    cursor = db.connection.cursor()
+    query = "SELECT * FROM Users WHERE email = %s"
+    cursor.execute(query, (email))
+    user = cursor.fetchone()
 
-    return jsonify(user_data)
+    if user is None:
+        response = {"error": "User does not exist"}
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+
+    username = user[1]
+    folders = []
+    sequences = []
+
+    query = "SELECT folder_id, display_name, created FROM Folders WHERE owner = %s"
+    cursor.execute(query, (email))
+    raw_folders = cursor.fetchall()
+
+    for raw_folder in raw_folders:
+        folder_id = raw_folder[0]
+        display_name = raw_folder[1]
+        created = raw_sequence[2]
+
+        folder = {
+            "id": folder_id,
+            "display_name": display_name,
+            "created": created,
+            "sequences": [],
+        }
+
+        query = "SELECT sequence FROM Contains WHERE folder = %s"
+        cursor.execute(query, (email))
+        folder_sequences = cursor.fetchall()
+
+        for sequence in folder_sequences:
+            sequence_id = sequence[0]
+            folder[sequences].append(sequence_id)
+
+        folders.append(folder)
+
+    query = "SELECT sequence_id, bpm, display_name, filename, created FROM Sequences WHERE creator = %s"
+    cursor.execute(query, (email))
+    raw_sequences = cursor.fetchall()
+
+    for raw_sequence in raw_sequences:
+        sequence_id = raw_sequence[0]
+        bpm = raw_sequence[1]
+        display_name = raw_sequence[2]
+        filename = raw_sequence[3]
+        created = raw_sequence[4]
+
+        with open(f'{NOTE_DATA_PATH}/{filename}.txt', 'r') as f:
+            notes = f.read()
+
+            sequence = {
+                "id": sequence_id,
+                "bpm": bpm,
+                "display_name": display_name,
+                "created": created,
+                "notes": notes,
+            }
+
+            sequences.append(sequence)
+
+    user_data = {
+        "username": username,
+        "folders": folders,
+        "sequences": sequences,
+    }
+
+    response = jsonify(user_data)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
+@app.route('/get-recording-file/<sequence-id>', methods=['GET'])
+def get_recording_file(sequence_id):
+    """Fetches the MP3 file corresponding to a recorded sequence.
+
+    Parameters
+    ----------
+    sequence_id : int
+        The sequence to be retrieved
+
+    Returns
+    -------
+    MP3 response
+        The recorded sequence as an MP3 file
+    """
+    cursor = db.connection.cursor()
+    query = "SELECT display_name, filename FROM Sequences WHERE sequence_id = %s"
+    cursor.execute(query, (sequence_id))
+    sequence = cursor.fetchone()
+    cursor.close()
+
+    if sequence is None:
+        response = jsonify({"error": "Sequence does not exist"}), 400
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+    
+    display_name = sequence[0]
+    filename = sequence[1]
+    path = f'{AUDIO_DATA_PATH}/{filename}.mp3'
+    response = send_file(path, as_attachment=True, attachment_filename=f'{display_name}.mp3')
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 
 @app.route('/process-recording', methods=['POST'])
@@ -137,16 +238,34 @@ def rename_sequence():
     display_name = data.get('display_name')
 
     if sequence_id is None or not isinstance(sequence_id, int):
-        # TODO also validate that sequence ID exists as a record in database
-        return jsonify({"error": "Missing or invalid sequence ID"}), 400
+        response = jsonify({"error": "Missing or invalid sequence ID"}), 400
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+    
+    cursor = db.connection.cursor()
+    query = "SELECT * FROM Sequences WHERE sequence_id = %s"
+    cursor.execute(query, (sequence_id))
+    sequence = cursor.fetchone()
+
+    if sequence is None:
+        cursor.close()
+        response = jsonify({"error": f"Sequence {sequence_id} does not exist"}), 400
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
 
     if display_name is None:
-        return jsonify({"error": "Missing new display name"}), 400
+        cursor.close()
+        response = jsonify({"error": "Missing new display name"}), 400
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
 
-    # TODO process edit to sequence name, save to database
-    query = client.update_sequence_data(sequence_id, {'display_name': display_name})
-
-    return jsonify({"message": f"Sequence {sequence_id} renamed to {display_name} successfully"})
+    query = "UPDATE Sequences SET display_name = %s WHERE sequence_id = %s"
+    cursor.execute(query, (display_name, sequence_id))
+    db.connection.commit()
+    cursor.close()
+    response = jsonify({"message": f"Sequence {sequence_id} renamed to {display_name} successfully"})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 
 @app.route('/update-sequence-data', methods=['PUT'])
@@ -180,48 +299,25 @@ def update_sequence_data():
     return jsonify({"message": f"Sequence {sequence_id} updated successfully"})
 
 
-@app.route('/create-folder', methods=['POST'])
-def create_folder():
+@app.route('/create-folder/<display_name>/<owner>', methods=['POST'])
+def create_folder(display_name, owner):
     """
     TODO complete docstring
     -------
     JSON response
         A JSON response containing ...
     """
-
-    if not request.is_json:
-        return jsonify({"error": "Invalid request format"}), 400  # TODO make more descriptive
-
-    data = request.get_json()
-
-    owner = data.get('owner')
-    display_name = data.get('display_name')
-    sequences = data.get('sequences')  # can be initially empty
-
-    if owner is None:
-        return jsonify({"error": "Missing folder owner"}), 400
-
-    if display_name is None:
-        return jsonify({"error": "Missing folder name"}), 400
-
-    if sequences is None:
-        return jsonify({"error": "Missing folder sequences"}), 400
-
-    if not isinstance(sequences, list) or not all(isinstance(sequence_id, int) for sequence_id in sequences):
-        # TODO also verify all sequence IDs exist in database
-        return jsonify({"error": "Invalid sequence IDs"}), 400
-
-    # TODO create folder in database
-    query = client.create_folder(owner, display_name)
-    folder_id = -1 # folder id assigned by the database to the new folder
-    for sequence in sequences:
-        client.add_sequence_to_folder(sequence, folder_id)
+    cursor = db.connection.cursor()
+    query = "INSERT INTO Folders (display_name, owner) VALUES (%s, %s)"
+    cursor.execute(query, (display_name, owner))
+    db.connection.commit()
+    cursor.close()
+    response = jsonify({"message": f"{display_name} created for {owner} successfully"})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 
-    return jsonify({"message": f"{display_name} created for {owner} successfully"})
-
-
-@app.route('/rename-folder', methods=['PATCH'])  # TODO change method to PUT
+@app.route('/rename-folder', methods=['PUT'])
 def rename_folder():
     """
     TODO complete docstring
@@ -231,79 +327,168 @@ def rename_folder():
     """
 
     if not request.is_json:
-        return jsonify({"error": "Invalid request format"}), 400  # TODO make more descriptive
+        response = jsonify({"error": "Invalid request format"}), 400  # TODO make more descriptive
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
 
     data = request.get_json()
     folder_id = data.get('folder_id')
     display_name = data.get('display_name')
 
     if folder_id is None:
-        return jsonify({"error": "Missing folder ID"}), 400
+        response = jsonify({"error": "Missing folder ID"}), 400
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
 
     if display_name is None:
-        return jsonify({"error": "Missing new folder name"}), 400
+        response = jsonify({"error": "Missing new folder name"}), 400
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
 
-    # TODO verify that folder ID exists
+    cursor = db.connection.cursor()
+    query = "SELECT * FROM Folders WHERE folder_id = %s"
+    cursor.execute(query, (folder_id))
+    folder = cursor.fetchone()
 
-    original_name = ''  # TODO get original display name from database
+    if folder is None:
+        cursor.close()
+        response = jsonify({"error": "Folder does not exist in database"}), 400
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
 
-    # TODO update folder name in database
-    query = client.update_folder_data(folder_id, {'display_name': display_name})
-
-    return jsonify({"message": f"{original_name} renamed to {display_name} successfully"})
+    original_name = folder[1]
+    query = "UPDATE Folders SET display_name = %s WHERE folder_id = %s"
+    cursor.execute(query, (display_name, folder_id))
+    db.connection.commit()
+    cursor.close()
+    response = jsonify({"message": f"{original_name} renamed to {display_name} successfully"})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 
 @app.route('/update-folder-contents', methods=['PUT'])
 def update_folder_contents():
     """
     TODO complete docstring
+    TODO replace with add and delete routes
     -------
     JSON response
         A JSON response containing ...
     """
 
     if not request.is_json:
-        return jsonify({"error": "Invalid request format"}), 400  # TODO make more descriptive
+        response = jsonify({"error": "Invalid request format"}), 400  # TODO make more descriptive
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
 
     data = request.get_json()
     folder_id = data.get('folder_id')
     sequences = data.get('sequences')
 
     if folder_id is None:
-        return jsonify({"error": "Missing folder ID"}), 400
+        response = jsonify({"error": "Missing folder ID"}), 400
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
 
     if sequences is None:
-        return jsonify({"error": "Missing folder sequences"}), 400
+        response =  jsonify({"error": "Missing folder sequences"}), 400
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
 
     if not isinstance(sequences, list) or not all(isinstance(sequence_id, int) for sequence_id in sequences):
-        # TODO also verify all sequence IDs exist in database
-        # TODO verify that all sequences belong to the user whos folder it is
-        return jsonify({"error": "Invalid sequence IDs"}), 400
+        response = jsonify({"error": "Invalid sequence IDs"}), 400
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+    
+    cursor = db.connection.cursor()
+    query = "SELECT * FROM Folders WHERE folder_id = %s"
+    folder = cursor.execute(query, (folder_id))
 
-    # TODO verify that folder ID exists
+    if folder is None:
+        cursor.close()
+        response = jsonify({"error": f"Folder does not exist"}), 400
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+    
+    folder_owner = folder[2]
 
-    # TODO update "Contains" table in database
-    for sequence in sequences:
-        query = client.add_sequence_to_folder(sequence, folder_id)
+    for sequence_id in sequences:
+        query = "SELECT creator FROM Sequences WHERE sequence_id = %s"
+        cursor.execute(query, (sequence_id))
+        sequence = cursor.fetchone()
 
-    display_name = ''  # TODO get display name from database
-    query = client.get_folder_data(folder_id) # extract 'display_name' fromt he result of this query
+        if sequence is None:
+            cursor.close()
+            response = jsonify({"error": f"Sequence {sequence_id} does not exist"}), 400
+            response.headers.add('Access-Control-Allow-Origin', '*')
+            return response
+        
+        sequence_owner = sequence[0]
 
-    return jsonify({"message": f"{display_name} updated successfully"})
+        if sequence_owner != folder_owner:
+            cursor.close()
+            response = jsonify({"error": f"Sequence {sequence_id} is not owned by {folder_owner}"}), 400
+            response.headers.add('Access-Control-Allow-Origin', '*')
+            return response
+        
+    query = "SELECT sequence FROM Contains WHERE folder = %s"
+    cursor.execute(query, (folder_id))
+    current_sequences = folder.fetchall()
+    current_sequence_ids = [sequence[0] for sequence in current_sequences]
 
-# TODO add routes to delete sequences and folders
+    for sequence_id in sequences:  # add newly added sequences
+        if sequence_id not in current_sequence_ids:
+            query = "INSERT INTO Contains (folder, sequence) VALUES (%s, %s)"
+            cursor.execute(query, (folder_id, sequence_id))
+
+    for sequence_id in current_sequence_ids:  # remove newly removed sequences
+        if sequence_id not in sequences:
+            query = "DELETE FROM Contains WHERE sequence = %s"
+            cursor.execute(query, (sequence_id))
+ 
+    cursor.close()
+    display_name = folder[1]
+    response = jsonify({"message": f"{display_name} updated successfully"})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 
-@app.route('/create_user/<email>/<username>', methods=['POST'])
+@app.route('/delete-folder/<folder_id>', methods=['DELETE'])
+def delete_folder(folder_id):
+    cursor = db.connection.cursor()
+    query = "DELETE FROM Folders WHERE folder_id = %s"
+    cursor.execute(query, (folder_id))
+    query = "DELETE FROM Contains WHERE folder = %s"
+    cursor.execute(query, (folder_id))
+    db.connection.commit()
+    cursor.close()
+    response = jsonify({"message": f"Database updated successfully"})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
+@app.route('/delete-sequence/<sequence_id>', methods=['DELETE'])
+def delete_sequence(sequence_id):
+    cursor = db.connection.cursor()
+    query = "DELETE FROM Sequences WHERE sequence_id = %s"
+    cursor.execute(query, (sequence_id))
+    query = "DELETE FROM Contains WHERE sequence = %s"
+    cursor.execute(query, (sequence_id))
+    db.connection.commit()
+    cursor.close()
+    response = jsonify({"message": f"Database updated successfully"})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
+@app.route('/create-user/<email>/<username>', methods=['POST'])
 def create_user(email, username):
-    query = client.create_user(email, username)
-    result = execute_query(query)
-    if result:     
-        # TODO in future, rollback DB in case of errors
-        message = f"Database updated successfully; new user ({username}) created"
-    else: # database query didn't succeed
-        message = f"Database couldn't create new user ({username})"
-    response = jsonify({"message": message})
+    cursor = db.connection.cursor()
+    query = "INSERT INTO User (email, display_name) VALUES (%s, %s)"
+    cursor.execute(query, (email, username))
+    db.connection.commit()
+    cursor.close()
+    response = jsonify({"message": f"Database updated successfully"})
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
